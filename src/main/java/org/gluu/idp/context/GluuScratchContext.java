2a282755f3675fdd03a7a1eee1242f90485ea081
Djeumen Rolain <uprightech@gmail.com>
2021-07-05 12:49:16 +0100

    fix: Attribute translator integration
    Added description to GluuScratchContext

--- src/main/java/org/gluu/idp/context/GluuScratchContext.java
+++ src/main/java/org/gluu/idp/context/GluuScratchContext.java
@@ -6,6 +6,13 @@ import org.opensaml.messaging.context.BaseContext;
 
 import net.shibboleth.idp.attribute.IdPAttribute;
 
+/**
+ * Scratch context to pass data between the servlet and Shibboleth IDP
+ * Currently being used only by the post-processing script, but could 
+ * be used in other places
+ * @author Djeumen Rolain
+ * @version 0.1, 07/05/2021
+ */
 public final class GluuScratchContext extends BaseContext{
     
     private List<IdPAttribute> idpAttributes;
